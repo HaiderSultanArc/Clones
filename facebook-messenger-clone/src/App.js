c17304233ee5595c6b92c80ad0501eae6400d606
Haider-Sultan-arc <haidersultanarc@gmail.com>
2020-11-09 01:40:45 +0500

    Messages Animations Added

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import { FormControl, InputLabel, Input, IconButton } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
 import db from './firebase';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
   // useState() hook is basically just set a state, it's like declaring a variable
@@ -49,26 +50,31 @@ function App() {
 
   return (
     <div className="App">
+      <img className="app__icon" src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="Messenger" />  {/* w=100&h=100 at the end is actually the dimensions of image that we want */}
       <h2>Welcome {username}</h2>
 
-      <form>  {/* Form can be used to click the button using enter key, changing type of button to submit shall work but forms refresh on submit, we can change that by preventing default behaviour of event */}
-        <FormControl>
-          <InputLabel>Enter a Message...</InputLabel> {/* Material UI Input Label */}
-          <Input value={input} onChange={event => setInput(event.target.value)} />  {/* value is set to the state input, onChange is set to the event that firesoff the function setInput that will set the value of state */}
-          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send</Button> {/* Button is disabled when there's no input, type of Button is submit, onClick will fire of the function sendMessage() */}
+      <div className="app__messagesArea">
+        <FlipMove>
+          {
+            messages.map( // messages.map() will map through all the messages
+              ({ id, fields }) => (
+                <Message key={id} username={username} fields={fields} /> // key is important when we need to render only the object that has a unique key. By introducing this key React Componenet will only re-render those elements that has unique key
+              )
+            )
+          }
+        </FlipMove>
+      </div>
+
+      <form className="app__form">  {/* Form can be used to click the button using enter key, changing type of button to submit shall work but forms refresh on submit, we can change that by preventing default behaviour of event */}
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder="Enter a Message..." value={input} onChange={event => setInput(event.target.value)} />  {/* value is set to the state input, onChange is set to the event that firesoff the function setInput that will set the value of state */}
+
+          <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>  {/* Button is disabled when there's no input, type of Button is submit, onClick will fire of the function sendMessage() */}
+            <SendIcon />
+          </IconButton>
+
         </FormControl>
       </form>
-
-      <FlipMove>
-        {
-          messages.map( // messages.map() will map through all the messages
-            ({id, fields}) => (
-              <Message key={id} username={username} fields={fields} /> // key is important when we need to render only the object that has a unique key. By introducing this key React Componenet will only re-render those elements that has unique key
-            )
-          )
-        }
-      </FlipMove>
-
     </div>
   );
 }
--- facebook-messenger-clone/src/Message.js
+++ facebook-messenger-clone/src/Message.js
@@ -1,18 +1,26 @@
 import { Card, CardContent, Typography } from '@material-ui/core'
-import React, {forwardRef} from 'react'
+import React, { forwardRef, useEffect, useRef} from 'react'
 import './Message.css';
 
 const Message = forwardRef( // forwardRef is a higher order function, it just wraps what we already have with some additional functionalities, which in this case is ref (reference)
     ({fields, username}, ref) => {   // props that were passed to the component
         const isUser = username ===fields.username;   // username is the logged in user and message.username is the username who sent the message
-        
+        const messagesEndRef = useRef(null);
+
+        useEffect(
+            () => {
+                messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+            }, [fields]
+        );
+
         return (
             <div ref={ref} className={`message ${isUser && 'message__user'}`}>    {/* ref={ref} is reference of the object to use animations className is message and if the username === message.username then className is also message__user so only person who is logged in will get both classes for him only */}
-                <Card className={isUser? "message__userCard": "message__questCard"}>  {/* if user is logged in then message__userCard is className else message__guestCard */}
+                <Card className={isUser? "message__userCard": "message__guestCard"}>  {/* if user is logged in then message__userCard is className else message__guestCard */}
                     <CardContent>
                         <Typography color="white" variant="h5" component="h2">
-                            {fields.username}: {fields.message}   {/* fields.username is username and fields.message are the fields of individual document that were sent to react component by mapping through all the collections 'messages' */}
+                            {!isUser && `${fields.username || 'Unknown User'}: `} {fields.message}   {/* fields.username is username and fields.message are the fields of individual document that were sent to react component by mapping through all the collections 'messages' */}
                         </Typography>
+                        <div ref={messagesEndRef} />
                     </CardContent>
                 </Card>
             </div>
